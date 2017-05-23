5c7a7afc94165f3848898e0a43b224cafa1fba50
ronscoder <rons.ubm@gmail.com>
2017-05-23 16:30:26 +0530

    gpx parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import data from './data.json';
-
+import { Http } from '@angular/http';
+import { DateserviceService } from './dateservice.service';
 // declare var XLSX: any;
 
-
+declare var google: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +13,15 @@ import data from './data.json';
 export class AppComponent implements OnInit {
   title = 'app works!';
   showResult = false;
+
+  constructor(
+    private http: Http,
+    private data: DateserviceService
+  ) { }
   ngOnInit() {
     // console.log(data);
     // console.log(result_json);
+    // this.testlocation();
   }
   showRenderedMap() {
     this.showResult = !this.showResult;
@@ -24,5 +31,25 @@ export class AppComponent implements OnInit {
 
   }
 
-
+  testlocation() {
+    navigator.geolocation.watchPosition((position) => {
+      console.log(position);
+      new google.maps.Map(document.getElementById('map2'), {
+        center: { lat: position.coords.latitude, lng: position.coords.longitude },
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+    }, error => {
+      console.log(error.message)
+    })
+  }
+  handleFiles(files) {
+    // this.http.get(files[0]).subscribe(res => {
+    //   console.log(res.text)
+    // })
+    const file = files[0];
+    this.data.parseXML(file, (result) => {
+      console.log(result);
+    })
+  }
 }
--- src/app/dateservice.service.ts
+++ src/app/dateservice.service.ts
@@ -5,4 +5,31 @@ export class DateserviceService {
   mapData: any;
   constructor() { }
 
+  parseXML(file, cb) {
+    const reader = new FileReader();
+    reader.addEventListener('loadend', () => {
+      const xmldata = reader.result;
+      cb(this.getGPS(xmldata));
+    });
+    reader.readAsText(file);
+  }
+
+  getGPS(xmltext: string): any[] {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(xmltext, 'text/xml');
+    const gpx = doc.getElementsByTagName('gpx')[0];
+    const wpts = gpx.getElementsByTagName('wpt');
+    let gpsdata = [];
+    for (let i = 0; i < wpts.length; i++) {
+      const wpt = wpts[i];
+      gpsdata.push(
+        {
+          latlng: { lat: wpt.getAttribute('lat'), lng: wpt.getAttribute('lon') },
+          name: wpt.getElementsByTagName('name')[0].childNodes[0].nodeValue,
+          time: wpt.getElementsByTagName('time')[0].childNodes[0].nodeValue
+        }
+      )
+    }
+    return gpsdata;
+  }
 }
